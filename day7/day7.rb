6608e98907e05a47b3100de9571fb96823ba5186
Robert Detjens <detjensrobert@gmail.com>
2020-12-12 00:05:55 -0800

    remove global variables from day 7
    Signed-off-by: Robert Detjens <detjensrobert@gmail.com>

--- day7/day7.rb
+++ day7/day7.rb
@@ -4,7 +4,7 @@
 rule_strs = File.read('./input').split("\n")
 
 # hash of color => valid contained colors
-$rules = {}
+rules = {}
 
 rule_strs.each do |r|
   bag, contains = r.split(/(.+) bags contain (.+) bags?./).drop(1)
@@ -18,41 +18,40 @@ rule_strs.each do |r|
                end.to_h
              end
 
-  $rules[bag] = contains
+  rules[bag] = contains
 end
 
 # == Part 1 ==
 # How many colors of bags can contain a shiny gold bag?
 
-$already_known = {}
-def can_contain_gold?(bag_color)
-  return $already_known[bag_color] if $already_known.include? bag_color # memoization ftw
+def can_contain_gold?(rules, already_known, bag_color)
+  return already_known[bag_color] if already_known.include? bag_color # memoization ftw
 
-  return false if $rules[bag_color].nil? # nil == cannot contain any bags
+  return false if rules[bag_color].nil? # nil == cannot contain any bags
 
-  return true if $rules[bag_color].include? 'shiny gold' # stop recursing if we can hold it
+  return true if rules[bag_color].include? 'shiny gold' # stop recursing if we can hold it
 
-  $already_known[bag_color] = $rules[bag_color].map { |r| can_contain_gold?(r[0]) }.any? # recurse if not found
-  $already_known[bag_color]
+  already_known[bag_color] = rules[bag_color].map { |r| can_contain_gold?(rules, already_known, r[0]) }.any? # recurse if not found
+  already_known[bag_color]
 end
 
 total = 0
-$rules.each do |color, _contains|
-  total += 1 if can_contain_gold?(color)
+rules.each do |color, _contains|
+  total += 1 if can_contain_gold?(rules, {}, color)
 end
 puts "Total bags that can contain a gold bag: #{total}"
 
 # == Part 2 ==
 # How many bags are contained within a shiny gold bag?
 
-def bag_contains(bag_color)
+def bag_contains(rules, bag_color)
   total = 1 # self
-  return total if $rules[bag_color].nil?
+  return total if rules[bag_color].nil?
 
-  $rules[bag_color].each do |bag, count|
-    total += count.to_i * bag_contains(bag)
+  rules[bag_color].each do |bag, count|
+    total += count.to_i * bag_contains(rules, bag)
   end
   total
 end
 
-puts "Total bags within a gold bag: #{bag_contains('shiny gold') - 1}"
+puts "Total bags within a gold bag: #{bag_contains(rules, 'shiny gold') - 1}"
